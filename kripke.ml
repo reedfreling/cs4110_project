7db4c2894073cf61aa45a12fa3fafdee22d2c639
Samuel Levenson <shl96@cornell.edu>
2020-12-04 00:56:35 -0500

    remove dependencies on eval and set from kripke

--- kripke.ml
+++ kripke.ml
@@ -1,40 +1,37 @@
-open Set
 open Ast
 
-type var = string
-
-type world = string
-
 (* (w1, w2) in W <=> w2 is accessible from w1 *)
-type access_relation = (world * world) set
+type access_relation = (world * world) list
 
 (* if a world appears in the list associated to some propositional var
   we consider that variable to be true in that world
  *)
-type valuation_function = (var * world set) set
+type valuation_function = (var * world list) list
 
-type kripke = (world set * access_relation * valuation_function)
+type kripke = (world list * access_relation * valuation_function)
+
+let list_union l1 l2 = List.fold_left (fun acc elt -> if List.mem elt acc then acc else elt::acc) l1 l2
 
 (* start off with empty model *)
-let empty = (Set.empty, Set.empty, Set.empty)
+let empty = ([], [], [])
 
 let add_worlds (k : kripke) (w : world) =
   match k with
-  | (w', r, v) -> let w_set = Set.singleton w in (union w_set w', r, v)
+  | (worlds, r, v) -> ((list_union [w] worlds), r, v)
 
 let add_accessibility (k : kripke) (world_pair : world * world) =
   match k with
-  | (w, r, v) -> (w, Set.add_elt r world_pair, v)
+  | (w, r, v) -> (w, world_pair::r, v)
 
 let add_valuation (k : kripke) (x : var) (w : world) =
-  let w_set = Set.singleton w in
+  let w_set = [w] in
   match k with
   | (worlds, r, v) -> 
-    let current_valuation = Set.assoc_opt x v in
+    let current_valuation = List.assoc_opt x v in
     (
       match current_valuation with
       | None -> (worlds, r, (x, w_set)::v)
-      | Some curr_val -> (worlds, r, (x, union w_set curr_val)::(Set.remove_assoc x v))
+      | Some curr_val -> (worlds, r, (x, list_union w_set curr_val)::(List.remove_assoc x v))
     )
 
 (* check if w2 is accessible from w1 *)
@@ -45,7 +42,7 @@ let rec check_accessibility (m : kripke) (w1 : world) (w2 : world) =
       match w with
       | None -> false
       | Some w'-> if w2 = w' then true else 
-        let r' = Set.remove_assoc w1 r in 
+        let r' = List.remove_assoc w1 r in 
         let m' = (worlds, r', v) in
         check_accessibility m' w1 w2
     )
@@ -54,30 +51,24 @@ let rec check_accessibility (m : kripke) (w1 : world) (w2 : world) =
 let rec accessible_worlds (m : kripke) (w : world) = 
   match m with
   | (w'::worlds, r, v) -> let m' = (worlds, r, v) in
-    if check_accessibility m w w' then Set.add_elt (accessible_worlds m' w) w'
+    if check_accessibility m w w' then w'::(accessible_worlds m' w)
     else (accessible_worlds m' w)
-  | ([], r, v) -> Set.empty
+  | ([], r, v) -> []
 
-(* let rec world_to_store (m : kripke) (w : world) : Eval.store = 
-  match m with
-  | (worlds, r, v) -> List.map (fun (k, v') -> if List.mem w v' then (k, True) else (k, False)) v
+let rec eval_bexp (m : kripke) (w : world) (e : bexp) : bexp = 
+  failwith "todo"
 
-let rec world_to_var_store (m : kripke) (w : world) : Eval.var_store = 
-  let var_sigma, _ = List.split (world_to_store m w) in var_sigma *)
-
-(* let rec check_valuation_at_world (m : kripke) (w : world) (e : mexp) : bexp = 
+let rec check_valuation_at_world (m : kripke) (w : world) (e : mexp) : bexp = 
   match e with
   | Bexp e' -> 
-    let sigma = world_to_store m w in
-    let var_sigma = world_to_var_store m w in
-    Eval.beval sigma var_sigma e'
+    eval_bexp m w e'
   | Square e' -> 
     let a_worlds = accessible_worlds m w in
     true_at_all m a_worlds e'
   | Diamond e' -> 
     let a_worlds = accessible_worlds m w in
     true_at_some m a_worlds e'
-and true_at_all (m : kripke) (worlds : world set) (e : mexp) =
+and true_at_all (m : kripke) (worlds : world list) (e : mexp) =
   match e with
   | Bexp e' -> 
     (
@@ -101,7 +92,7 @@ and true_at_all (m : kripke) (worlds : world set) (e : mexp) =
         And ((true_at_some m a_worlds e'), (true_at_all m worlds' e'))
       | [] -> True
     )
-and true_at_some (m : kripke) (worlds : world set) (e : mexp) = 
+and true_at_some (m : kripke) (worlds : world list) (e : mexp) = 
   match e with
   | Bexp e' -> 
     (
@@ -126,15 +117,11 @@ and true_at_some (m : kripke) (worlds : world set) (e : mexp) =
       | [] -> False
     )
 
-let eval_mexp (m : kripke) (w : world) (e : mexp) : bool = 
+let eval_mexp (m : kripke) (w : world) (e : mexp) : bexp = 
   match m with
   | (worlds, r, v) ->
-    let sigma = world_to_store m w in
-    let var_sigma = world_to_var_store m w in
-    (* possibly sigma and var_sigma could both be [] because check_valuation_at_world 
-    should only return a bexp containing {True, False, And, Or} *)
-    let b = Eval.beval sigma var_sigma (check_valuation_at_world m w e) in
+    let b = eval_bexp m w (check_valuation_at_world m w e) in
     match b with
-    | True -> true
-    | False -> false
-    | _ -> failwith "e could not be evaluated to T/F" *)
+    | True -> True
+    | False -> False
+    | _ -> failwith "e could not be evaluated to T/F"
